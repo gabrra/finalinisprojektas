43c11f905ca1f3069f209c962c68339bdf8dabca
Gabriele Radaviciute <gabrieleradaviciute@Gabrieles-MacBook-Air.local>
2024-08-12 20:19:02 +0300

    updating info

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
-from scraper_h3 import get_sorted_titles # change to scraper_h3 if economist or lrt or others
+from scraper import get_sorted_titles
 
-def print_sorted_titles(url: str, class_name: str):
-    sorted_titles = get_sorted_titles(url, class_name)
+
+def print_sorted_titles(url: str, tag_name: str, class_name: str):
+    sorted_titles = get_sorted_titles(url, tag_name, class_name)
     for title in sorted_titles:
         print(title)
 
+
 def main():
-    # Prompt user for URL and class name
-    url = input("Enter the URL of the website to scrape: ")
-    class_name = input("Enter the class name of the HTML element to scrape: ")
-    
-    # Print sorted titles 
-    print_sorted_titles(url, class_name)
+    print("Choose website to scrape:")
+    print("1. BBC")
+    print("2. CNN")
+    print("3. Economist")
+    print("4. LRT")
+    print("5. 15min")
+
+    choice = input("Enter website number to scrape (1-5): ")
+
+    if choice == '1':
+        url = "https://www.bbc.com"
+        tag_name = "div"
+        class_name = "sc-4fedabc7-0 kZtaAl"
+    elif choice == '2':
+        url = "https://edition.cnn.com"
+        tag_name = "div"
+        class_name = "container__text container_lead-plus-headlines__text"
+    elif choice == '3':
+        url = "https://www.economist.com"
+        tag_name = "h3"
+        class_name = "css-1jzypbl e7j57mt0"
+    elif choice == '4':
+        url = "https://www.lrt.lt"
+        tag_name = "h3"
+        class_name = "news__title"
+    elif choice == '5':
+        url = "https://www.15min.lt"
+        tag_name = "h4"
+        class_name = "vl-title item-no-front-style"
+    else:
+        print("Please choose a number from 1 to 5")
+        return
+ 
+    print_sorted_titles(url, tag_name, class_name)
+
 
 if __name__ == "__main__":
     main()
-
-# https://www.bbc.com
-# sc-4fedabc7-0 kZtaAl
-
-# https://edition.cnn.com/
-# container__text container_lead-plus-headlines__text
-
-# https://www.economist.com/ #change from div to h3
-# css-1jzypbl e7j57mt0
-
-# https://www.lrt.lt/ #change from div to h3
-# news__title
-
-
--- readme.md
+++ readme.md
@@ -1,34 +1,36 @@
 # Bazinis scraping projektas
 
-Šis projektas skirtas baziniam interneto svetainės scrapinimui, kuriame nurodomas svetainės https adresaas ir straipsnių vieta, iš kurios bus scrapinama
+Šis projektas skirtas baziniam interneto svetainės scrapinimui, kuriame nurodomas svetainės https adresas ir straipsnių vieta, iš kurios bus scrapinama
 
 ## Prieš pradedant
 
 Turėtumėte įsitikinti, kad įrašytas BeautifulSoup https://pypi.org/project/beautifulsoup4/ 
 
 ## Failai
-- 'scraper.py' - pagrindinis scraperis, jei svetainėje naudojamas div elementas. 
-- 'scraper_h3.py' - papildomas scraperis, jei svetainėje naudojamas h3 elementas.
-- 'main.py' - failas, kuris turės būti paleistas Jūsų terminale. Šiame faile taip pat rasite svetainių ir jų straipsnių div pavyzdžių. Pastaba: jei naudosite h3, turėsite pakoreguoti ir main.py
-
+- 'scraper.py' - pagrindinis scraperis, jei svetainėje naudojamas div elementas
+- 'main.py' - failas, kuris turės būti paleistas terminale. Šiame faile taip pat rasite svetainių ir jų straipsnių div pavyzdžių. 
 ## Naudojimas
 
 Įsirašykite beautifulsoup4
-```bash
-pip install requests beautifulsoup4
+```pip install requests beautifulsoup4```
 
 Paleiskite 'main.py' failą
-```bash python main.py
+```python main.py```
 
 Pastaba, jei naudojate python3, turėtų būti
-```bash python3 main.py
+```python3 main.py```
 
-Sistema paprašys įvesti html, pavyzdys:
-```bash https://edition.cnn.com/
+Sistema paprašys terminale pasirinkti vieno iš penkių siūlomų html numerį (1-5):
+```Choose website to scrape:
+1. BBC
+2. CNN
+3. Economist
+4. LRT
+5. 15min
+```
 
-Tada spauskite enter ir įveskite elementą (atsiminkite, kas rašyta pradžioje dėl div ir h3)
-```bash container__text container_lead-plus-headlines__text
+Tada spauskite enter.
 
-Savo terminale matysite visą sąrašą straipsnių pavadinimų išrykiuotų pagal abecėlę
+Savo terminale matysite visą sąrašą straipsnių pavadinimų išrykiuotų pagal abecėlę.
 
 Gero naudojimo!
